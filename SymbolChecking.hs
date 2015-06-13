e1cff5d30a754a9e171cc53473a902586dcca680
Manuel Pacheco <manuelalejandropm@gmail.com>
2015-06-13 14:26:55 -0430

    completed Process instances of Statement and Range

--- SymbolChecking.hs
+++ SymbolChecking.hs
@@ -45,9 +45,59 @@ instance Process Statement where
     then process expr res
     -- iF its not, add an error and check the expression
     else
-      let extra = "Variable " ++ show iden ++ " is not declared in the scope."
+      let extra = "Variable " ++ show iden ++ " used in the left side of an \
+      \assignment is not declared in the scope."
       in process expr (Result (st, extra:out))
   -- #! TODO COMPLETE OTHER PATTERNS
+  process (Read (Identifier iden)) res@(Result (st, out)) = do
+    -- Check if the identifier is in scope (possibly not local)
+    sym <- ST.lookupComplete st iden
+    if isNothing sym
+    -- If it is, keep processing
+    then return res
+    -- iF its not, add an error and check the expression
+    else
+      let extra = "Variable " ++ show iden ++ " used in a Read statement is not \
+      \declared in the scope"
+      in return $ Result (st, extra:out)
+  process (Write (Identifier iden)) res@(Result (st, out)) = do
+    -- Check if the identifier is in scope (possibly not local)
+    sym <- ST.lookupComplete st iden
+    if isNothing sym
+    -- If it is, keep processing
+    then return res
+    -- iF its not, add an error and check the expression
+    else
+      let extra = "Variable " ++ show iden ++ " used in a Write statement is not \
+      \declared in the scope"
+      in return $ Result (st, extra:out)
+  process (If expr thenList elseList) res =
+    -- Just check the 3 components
+    process expr res >>= process thenList >>= process elseList
+
+  process (ForIn expr stList) res =
+    -- Just check the 2 components
+    process expr res >>= process stList
+  process (ForDet Nothing range stList) res = do
+    -- Check the range
+    newRes <- process range res
+    -- Check the statement list
+    process stList newRes
+  process (ForDet (Just ider@(Identifier iden)) range stList) res@(Result (st, out)) = do
+    -- Check the range first (it must not use the enumerator variable)
+    newRes <- process range res
+    -- Check the identifier in the local scope TODO verify scope rules
+    let (Result (newSt, newOut)) = newRes
+    sym <- ST.lookup newSt iden
+    let extra = "Variable " ++ show iden ++ " used as enumerated \
+    \identifier in For statement is already declared in the local scope"
+    nextRes <- if isJust sym
+    -- If found, add an error
+    then return $ Result (newSt, extra:newOut)
+    -- If its not, add it to the scope
+    else process (IntType, ider) newRes
+    -- Keep checking with the other pattern
+    process (ForDet Nothing range stList) nextRes
 
 -- Process variable declarations
 instance Process (DataType, Identifier) where
@@ -57,12 +107,22 @@ instance Process (DataType, Identifier) where
     if isJust sym
     -- If it is, add an error
     then
-      let extra = "Variable " ++ show iden ++ " is already declared in the scope."
+      let extra = "Variable " ++ show iden ++ " is already declared in the scope \
+      \ of a declaration list."
       in return $ Result (st, extra:out)
     -- If its not, add it to the table (hiding the upper one)
     else do
       newSt <- ST.insert st iden (ST.Symbol iden dt)
       return $ Result (newSt, out)
+
+-- Omitting processing DataTypes (not required)
+-- Ommiting processing Identifiers (they are embedded in other instances)
+
+-- Process range expressions
+instance Process Range where
+  process (Range e1 e2) res =
+    -- Just check both Expressions
+    process e1 res >>= process e2
 
 -- Process Expressions
 instance Process Expression where
